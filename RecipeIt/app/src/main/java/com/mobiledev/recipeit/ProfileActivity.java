f7f7da711f6f1dc15b719db13038c5a0fbb938f4
laolarou <luolinrui726@outlook.com>
2025-04-14 12:02:21 -0700

    finish profile page

--- RecipeIt/app/src/main/java/com/mobiledev/recipeit/ProfileActivity.java
+++ RecipeIt/app/src/main/java/com/mobiledev/recipeit/ProfileActivity.java
@@ -1,14 +1,18 @@
 package com.mobiledev.recipeit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+    private TextView profileName;
+    private TextView profileEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,37 @@ public class ProfileActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
 
+        // Initialize UI components
+        profileName = findViewById(R.id.profileNameText);
+        profileEmail = findViewById(R.id.profileEmailText);
+
+        // Set up the profile information
+        var auth = FirebaseAuth.getInstance();
+        var currentUser = auth.getCurrentUser();
+
+        if (currentUser != null) {
+            String name = currentUser.getDisplayName();
+            String email = currentUser.getEmail();
+
+            profileName.setText(name);
+            profileEmail.setText(email);
+        } else {
+            // Handle the case where the user is not logged in
+            profileName.setText("Guest");
+            profileEmail.setText("Not logged in");
+        }
+    }
+
+    public void onLogout(View v) {
+        profileName.setText("Guest");
+        profileEmail.setText("Not logged in");
+
+        var auth = FirebaseAuth.getInstance();
+        auth.signOut();
+
+        // Redirect to login activity
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
